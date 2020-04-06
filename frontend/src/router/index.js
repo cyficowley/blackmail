bee003545bbb83de0dbe20fad300b1ffd3ffd5e0
Cyrus Cowley <cyficowley@gmail.com>
2020-04-05 18:09:51 -0700

    adding more settings

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Landing from '../views/Landing.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    name: 'Landing',
+    component: Landing,
   },
   {
     path: '/about',
