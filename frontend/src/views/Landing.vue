86904cdf09476edf59703c893968b72e2e189638
supercarlos297 <footballshane@gmail.com>
2020-04-05 20:01:19 -0700

    finished sign up component

--- frontend/src/views/Landing.vue
+++ frontend/src/views/Landing.vue
@@ -4,6 +4,7 @@
     <v-content>
       <v-container class="fill-height" fluid>
         <Login/>
+        <Signup/>
       </v-container>
     </v-content>
   </v-app>
@@ -12,11 +13,14 @@
 <script>
 // @ is an alias to /src
 import Login from '@/components/Login.vue';
+import Signup from '@/components/Signup.vue';
 
 export default {
   name: 'Landing',
   components: {
     Login,
+    Signup,
+
   },
 };
 </script>
