2eb2622fb425ed72ee2b64b604a2e51d5f29cb21
Cyrus Cowley <cyficowley@gmail.com>
2020-04-05 18:37:33 -0700

    adding veutify

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Landing from '../views/Landing.vue';
+import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Landing',
-    component: Landing,
+    name: 'Home',
+    component: Home,
   },
   {
     path: '/about',
