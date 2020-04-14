4f2a4a9f28a11582d0bf3f2fb113b68f38dd5263
supercarlos297 <footballshane@gmail.com>
2020-04-13 22:53:08 -0700

    fixed random quote

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -122,7 +122,7 @@ export default {
     signOut() {
       this.$store.dispatch('signout');
     },
-˝
+
     ValidateEmail(mail) {
       // eslint-disable-next-line no-useless-escape
       if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
