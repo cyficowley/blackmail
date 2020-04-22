3361d60f8fa1e2add73ab8090d45aa7e31aaedb0
supercarlos297 <footballshane@gmail.com>
2020-04-21 23:08:24 -0700

    added fiver images + license

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,11 +6,32 @@
 
 <style>
 #app {
-  --dark-1: #EAE7DC;
-  --dark-2: #D8C3A5;
-  --light-1: #8E8D8A;
-  --light-2: #E98074;
-  --accent: #E85A4F;
+
+/* 4 */
+
+--dark-1: #303C6C;
+  --dark-2: #B4dFe5;
+  --light-1: #F27D42;
+  --light-2: #F4976C;
+  --accent: #D2FDFF;
+
+/*
+camp site
+--dark-1: #022449;
+  --dark-2: #B4dFe5;
+  --light-1: #F27D42;
+  --light-2: #558DCA;
+  --accent: #96FFFF; */
+  /* C8Eff9 */
+
+
+  /*
+  5???
+   --dark-1: #374785;
+  --dark-2: #24305E;
+  --light-1: #F76c6c;
+  --light-2: #F76c6c;
+  --accent: #A8D0E6; */
 
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -46,6 +67,7 @@
 
 h1, h2, h3, h4, h5, h6{
   font-family: 'Poppins', sans-serif;
+  color: white;
 }
 
 </style>
