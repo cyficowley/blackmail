a5042d91f054f4bc9b2287efc1a1a41f38621fe7
supercarlos297 <footballshane@gmail.com>
2020-05-11 22:22:54 -0700

    form link in footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -4,6 +4,8 @@
       <span style="white-space:nowrap;color:rgba(255,255,255,.6)">&copy; 2020 Blackmailer.xyz</span>
       <span style="white-space:nowrap;color:white; padding-left:20px;">
         Created by Shane Folden and Cyrus Cowley</span>
+        <a style="white-space:nowrap;color:white; padding-left:20px;"
+        href="https://docs.google.com/forms/d/e/1FAIpQLSdYiCTydQO68LIU5otXWmK1TB3f14wKVqQFkeqM6pmzGS5vCA/viewform?usp=sf_link">form</a>
     </v-col>
   </v-row>
 </template>
