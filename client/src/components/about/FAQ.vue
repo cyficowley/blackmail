e66be81c178a89dc90bd719d1b59b398dde0fe47
Andrew Young <andrew@youngshome.com>
2020-08-16 21:34:20 -0700

    FAQ added to about

---
+++ client/src/components/about/FAQ.vue
@@ -0,0 +1,159 @@
+<template>
+  <div
+    class="limit-width"
+    :class="$style.component">
+    <h1 :class="$style.top">
+      Privacy
+    </h1>
+
+    <v-expansion-panels
+      accordion
+      multiple
+      dark>
+      <v-expansion-panel
+        v-for="question in privacy"
+        class="light-1"
+        :key="question.q">
+        <v-expansion-panel-header color="accent">
+          <span :class="$style.question">
+            {{ question.q }}
+          </span>
+        </v-expansion-panel-header>
+
+        <v-expansion-panel-content style="text-align:left;">
+          <span :class="$style.answer">
+            {{ question.a}}
+          </span>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+
+    <h1>
+      Usage
+    </h1>
+
+    <v-expansion-panels
+      accordion
+      multiple
+      dark>
+      <v-expansion-panel
+        v-for="question in what"
+        class="light-1"
+        :key="question.q">
+        <v-expansion-panel-header color="accent">
+          <span :class="$style.question">
+            {{ question.q }}
+          </span>
+        </v-expansion-panel-header>
+
+        <v-expansion-panel-content style="text-align:left;">
+          <span :class="$style.answer">
+            {{ question.a}}
+          </span>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+
+    <h1>
+      Details
+    </h1>
+
+    <v-expansion-panels
+      accordion
+      multiple
+      dark>
+      <v-expansion-panel
+        v-for="question in logistics"
+        class="light-1"
+        :key="question.q">
+        <v-expansion-panel-header color="accent">
+          <span :class="$style.question">
+            {{ question.q }}
+          </span>
+        </v-expansion-panel-header>
+
+        <v-expansion-panel-content style="text-align:left;">
+          <span :class="$style.answer">
+            {{ question.a}}
+          </span>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FAQ',
+  data: () => ({
+    privacy: [
+      {
+        q: 'Do we ever look at your blackmail?',
+        a: 'No, the files are not viewable by your approvers. They are encrypted the entire time they are stored. Furthermore, the files are deleted immediatly once your proof has been aproved, or the deadline has passed.',
+      },
+      {
+        q: 'Is the blackmail email anonymous?',
+        a: 'No, we explain to the recipient why they are getting the file. They are provided your name and the task you failed to complete.',
+      },
+      {
+        q: 'Who approves the proof?',
+        a: 'All proof is reviewed by our internal team.',
+      },
+    ],
+    what: [
+      {
+        q: 'Is there any way to cancel a deadline?',
+        a: 'No, the only way to cancel your deadline is to submit valid proof.',
+      },
+      {
+        q: 'What happens if my proof gets rejected?',
+        a: 'If your proof is rejected, you will have at least 24 hours from the time of rejection to re-upload valid proof. You will be notified via email.',
+      },
+      {
+        q: 'Will you send me reminders?',
+        a: 'We email you a reminder a day before your goal, and on acceptance or rejection of your provided proof.',
+      },
+      {
+        q: 'Why was my proof rejected?',
+        a: 'We reject proof if it doesn\'t clearly show the completion of the goal. You can contact us if you have any question about why your specific proof was rejected.',
+      },
+    ],
+    logistics: [
+      {
+        q: 'What kind of blackmail can I upload?',
+        a: 'You can upload any type of file or write a message.',
+      },
+      {
+        q: 'What kind of proof can I submit?',
+        a: 'We accept proof in any format as long as it is under 5mb. The most important thing is that the proof clearly shows completiion of the task.',
+      },
+      {
+        q: 'Does the recipient need to be registered at blackmailer.xyz?',
+        a: 'No, you can enter the email of anyone you know.',
+      },
+    ],
+  }),
+};
+</script>
+
+<style module>
+.component{
+  margin: 24px auto;
+}
+
+.component h1 {
+  font-weight: 200;
+  font-size: 24px;
+  margin-top: 42px;
+  margin-bottom: 6px;
+}
+
+.component h1.top {
+  margin-top: 0;
+}
+
+.question {
+  margin: 6px 0;
+  text-align: left;
+}
+</style>
--- client/src/views/public/About.vue
+++ client/src/views/public/About.vue
@@ -1,45 +1,44 @@
 <template>
-  <div>
-    <a href="/#/home">
-      <v-btn :class="$style.button">
-        View Deadlines
-      </v-btn>
-    </a>
+  <div :class="$style.component">
+    <h1
+      class="limit-width"
+      :class="$style.title">
+      FREQUENTLY ASKED QUESTIONS
+    </h1>
 
-    <div
-      class="fullwindow"
-      style="z-index:0;">
-      <HowItWorks/>
-
-      <FAQ/>
-    </div>
+    <f-a-q />
   </div>
 </template>
 
 
 <script>
-import HowItWorks from '@/components/HowItWorks.vue';
-import FAQ from '@/components/FAQ.vue';
+import FAQ from '@/components/about/FAQ.vue';
 
 export default {
   name: 'About',
   components: {
-    HowItWorks,
     FAQ,
   },
 };
 </script>
 
 <style module>
-.button {
-  width: "8%";
-  position: fixed;
-  right: 20px;
-  top: 20px;
-  font-size:19px;
-  z-index: 99;
-  color:black;
-  font-weight: 700;
-  text-decoration: none;
+.component {
+  padding-bottom: 52px;
+}
+
+.component h1.title {
+  color: white;
+  font-weight: 400;
+  margin: 52px auto 0;
+  font-size: 42px;
+  text-align: center;
+}
+
+.component p {
+  text-align: center;
+  margin: 0 auto;
+  font-size: 18px;
+  color: rgba(255, 255, 255, 0.616);
 }
 </style>
